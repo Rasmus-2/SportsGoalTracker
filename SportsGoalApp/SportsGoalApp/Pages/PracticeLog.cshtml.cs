578378816e29e5196abe60db3f2c73a52d96455e
Mathias <mathiasakesson@hotmail.com>
2024-11-20 13:48:04 +0100

    initial

--- SportsGoalApp/SportsGoalApp/Pages/PracticeLog.cshtml.cs
+++ SportsGoalApp/SportsGoalApp/Pages/PracticeLog.cshtml.cs
@@ -5,8 +5,37 @@ namespace SportsGoalApp.Pages
 {
     public class PracticeLogModel : PageModel
     {
+        private readonly Data.SportsGoalAppContext _context;
+
+        public PracticeLogModel(Data.SportsGoalAppContext context)
+        {
+            _context = context;
+            PracticeLog = new Models.PracticeLog();
+        }
+
+        [BindProperty]
+        public Models.PracticeLog PracticeLog { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if(PracticeLog.TotalNumber.HasValue && PracticeLog.SuccessfulNumber.HasValue && PracticeLog.TotalNumber.Value > 0)
+            {
+                PracticeLog.Percentage = (float)PracticeLog.SuccessfulNumber / PracticeLog.TotalNumber * 100;
+            }
+
+            _context.Practices.Add(PracticeLog);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
