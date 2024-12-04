cd23f6eb03afc7bd76d6eecee72894a763e93bd0
Mathias <mathiasakesson@hotmail.com>
2024-12-04 13:23:16 +0100

    ReOrganised AiTEST

--- SportsGoalApp/SportsGoalAppTests/OpenAITests.cs
+++ SportsGoalApp/SportsGoalAppTests/OpenAITests.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 using OpenAIApi;
-using OpenAIAPITests.Mock;
+using SportsGoalAppTests.Mock;
 using System;
 using System.Collections.Generic;
 using System.Linq;
