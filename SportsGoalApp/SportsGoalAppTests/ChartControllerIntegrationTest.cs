e23df8786f3ba077195cc22a4c13b04e565c5bf2
Dwina83 <dwinalarsson@gmail.com>
2024-12-06 15:13:05 +0100

    Update ChartControllerIntegrationTest.cs
    Integration test is back again

--- SportsGoalApp/SportsGoalAppTests/ChartControllerIntegrationTest.cs
+++ SportsGoalApp/SportsGoalAppTests/ChartControllerIntegrationTest.cs
@@ -1,40 +1,40 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Net.Http;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc.Testing;
-//using Xunit;
-//using SportsGoalApp;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+using SportsGoalApp;
 
-//namespace SportsGoalAppTests
-//{
-//    public class ChartControllerIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
-//    {
-//        private readonly HttpClient _httpClient;
+namespace SportsGoalAppTests
+{
+    public class ChartControllerIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
 
-//        public ChartControllerIntegrationTest(WebApplicationFactory<Program> factory)
-//        { 
-//            _httpClient = factory.CreateClient();
-//        }
+        public ChartControllerIntegrationTest(WebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory.CreateClient();
+        }
 
-//        [Fact]
-//        public async Task GetChartValue_ReturnOk_WithValue()
-//        {
-//            //Arrange
-//            var request = "/api/chart/data";
+        [Fact]
+        public async Task GetChartValue_ReturnOk_WithValue()
+        {
+            //Arrange
+            var request = "/api/chart/data";
 
-//            //Act
-//            var response = await _httpClient.GetAsync(request);
+            //Act
+            var response = await _httpClient.GetAsync(request);
 
-//            //Assert
-//            response.EnsureSuccessStatusCode();
-//            var responseValue = await response.Content.ReadAsStringAsync();
-//            Assert.False(string.IsNullOrEmpty(responseValue));
+            //Assert
+            response.EnsureSuccessStatusCode();
+            var responseValue = await response.Content.ReadAsStringAsync();
+            Assert.False(string.IsNullOrEmpty(responseValue));
 
 
-//        }
-//    }
-//}
+        }
+    }
+}
