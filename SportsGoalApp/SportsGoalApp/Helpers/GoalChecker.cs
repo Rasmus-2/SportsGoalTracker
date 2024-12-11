cbe355982678ee9f7682ee876588bd8c15fd453f
Rasmus-2 <147603266+Rasmus-2@users.noreply.github.com>
2024-12-11 19:12:59 +0100

    Check Goal success
    Added a helper class that checks if a goal has been completed every time a new practice log is added to that goal.

---
+++ SportsGoalApp/SportsGoalApp/Helpers/GoalChecker.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SportsGoalApp.Models;
+
+namespace SportsGoalApp.Helpers
+{
+    public class GoalChecker
+    {
+        private UserManager<Areas.Identity.Data.SportsGoalAppUser> _userManager { get; set; }
+        private readonly Data.SportsGoalAppContext _context;
+
+        public GoalChecker(UserManager<Areas.Identity.Data.SportsGoalAppUser> userManager, Data.SportsGoalAppContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public async Task<bool> CheckIfAchieved(int? goalId)
+        {
+            List<PracticeLog> practiceLogs = await _context.Practices.Where(p => p.GoalId == goalId).ToListAsync();
+            if (practiceLogs.Count > 2)
+            {
+                practiceLogs = practiceLogs.OrderByDescending(p => p.DateTime).ToList();
+                if (practiceLogs.Count > 3)
+                {
+                    practiceLogs.RemoveRange(3, practiceLogs.Count - 3);
+                }
+
+                Goal goal = await _context.Goals.Where(g => g.Id == goalId).FirstOrDefaultAsync();
+                if (goal != null)
+                {
+                    float? goalNumber = goal.GoalNumber;
+                    foreach (var log in practiceLogs)
+                    {
+                        if (log.Percentage < goalNumber)
+                        {
+                            return false;
+                        }
+                    }
+                goal.Success = true;
+                _context.Goals.Update(goal);
+                await _context.SaveChangesAsync();
+                return true;
+                }
+            }
+            return false;
+        }
+    }
+}
