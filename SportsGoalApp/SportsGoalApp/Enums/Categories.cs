d5918c19fe12bdde2d3412a0a89dde2544281d85
matakesson <mathiasakesson@hotmail.com>
2024-12-11 19:28:57 +0100

    Added display name for categories

--- SportsGoalApp/SportsGoalApp/Enums/Categories.cs
+++ SportsGoalApp/SportsGoalApp/Enums/Categories.cs
@@ -1,12 +1,20 @@
-﻿namespace SportsGoalApp.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsGoalApp.Enums
 {
     public enum GoalCategories
     {
+        [Display(Name = "Skill improvement")]
         Skill_improvement,
+        [Display(Name = "Fitness")]
         Fitness,
+        [Display(Name = "Game sense")]
         Game_sense,
+        [Display(Name = "Mental training")]
         Mental_training,
+        [Display(Name = "Nutrition and Diet")]
         Nutrition_and_Diet,
+        [Display(Name = "Competition preperation")]
         Competition_preperation
     }
 }
